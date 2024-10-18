018644e55cf47f19cf0de77078c5299085f7f84b
LauraSirumapea <lauraawise07@gmail.com>
2024-10-18 10:27:52 +0700

    Pair12

--- M01.java
+++ M01.java
@@ -1,2 +1,62 @@
-// NIM - Your Name
-// NIM - Your Name
+// 12S24057 - Laura Awise Sirumapea
+// 12S24017 = Arya Pratama Sinambela
+
+import java.util.*;
+import java.lang.Math;
+
+public class M01 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        double a, aB, b, bC, c, d, e, nilaiangka, performa, kredit1;
+        String kredit, nilaihuruf;
+
+        kredit1 = 0;
+        nilaiangka = 4.0;
+        kredit = input.nextLine();
+        while (!kredit.equals("---")) {
+            if (kredit.equals("---")) {
+                kredit = kredit;
+            } else {
+                kredit1 = Integer.parseInt(kredit);
+            }
+            nilaihuruf = input.nextLine();
+            if (nilaihuruf.equals("A")) {
+                nilaiangka = 4.0;
+            } else {
+                if (nilaihuruf.equals("AB")) {
+                    nilaiangka = 3.5;
+                } else {
+                    if (nilaihuruf.equals("B")) {
+                        nilaiangka = 3.0;
+                    } else {
+                        if (nilaihuruf.equals("BC")) {
+                            nilaiangka = 2.5;
+                        } else {
+                            if (nilaihuruf.equals("C")) {
+                                nilaiangka = 2.0;
+                            } else {
+                                if (nilaihuruf.equals("D")) {
+                                    nilaiangka = 1.0;
+                                } else {
+                                    if (nilaihuruf.equals("E")) {
+                                        nilaiangka = 0.0;
+                                    } else {
+                                        nilaiangka = "";
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            performa = Integer.parseInt(kredit) * nilaiangka;
+            System.out.println(toFixed(performa,1));
+            kredit = input.nextLine();
+        }
+    }
+    
+    private static String toFixed(double value, int digits) {
+        return String.format("%." + digits + "f", value);
+    }
+}
